88dfc605f2aca15d2ebc5ab6fefea30a24961d11
Julian Gruber <julian@juliangruber.com>
2017-11-15 21:25:16 +0100

    some things are starting to work

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import level from 'level'
+import {List} from 'react-level-list'
+
+const db = window.db = level('/tmp/todomvc-react-leveldb-electron', {
+  valueEncoding: 'json'
+})
+
+class App extends React.Component {
+  onSubmit (ev) {
+    const data = new FormData(ev.target)
+    ev.preventDefault()
+    db.put(`${Date.now()}${Math.random()}`, {
+      completed: false,
+      text: data.get("text")
+    })
+  }
+  render () {
+    return (
+      <section>
+  			<header className="header">
+  				<h1>todos</h1>
+          <form onSubmit={this.onSubmit}>
+    				<input className="new-todo" placeholder="What needs to be done?" name="text" autoFocus />
+            <input type="submit" hidden />
+          </form>
+  			</header>
+  			<section className="main">
+  				<input id="toggle-all" className="toggle-all" type="checkbox" />
+  				<label htmlFor="toggle-all">Mark all as complete</label>
+  				<ul className="todo-list">
+            <List
+              db={db}
+              renderRow={({ key, value }) =>
+                <li className={value.completed ? "completed" : undefined} key={key}>
+                  <div className="view">
+      							<input className="toggle" type="checkbox" defaultChecked={value.completed} />
+      							<label>{value.text}</label>
+      							<button className="destroy"></button>
+                  </div>
+                  <input className="edit" defaultValue="TODO?" />
+                </li>
+              } />
+  				</ul>
+  			</section>
+  			<footer className="footer">
+  				<span className="todo-count"><strong>0</strong> item left</span>
+  				<ul className="filters">
+  					<li>
+  						<a className="selected" href="#/">All</a>
+  					</li>
+  					<li>
+  						<a href="#/active">Active</a>
+  					</li>
+  					<li>
+  						<a href="#/completed">Completed</a>
+  					</li>
+  				</ul>
+  				<button className="clear-completed">Clear completed</button>
+  			</footer>
+      </section>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('section.todoapp'))
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './src/index.js',
+  target: 'electron',
+  output: {
+    filename: 'static/build.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          `${__dirname}/node_modules/react-level-list`,
+          `${__dirname}/src`
+        ],
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  externals: {
+    leveldown: true
+  }
+}
