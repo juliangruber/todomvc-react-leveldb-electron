f103cbd5ade9a6344b5ba02639f8241ee81d1021
Julian Gruber <julian@juliangruber.com>
2017-12-18 15:57:58 +0100

    move from label to input

--- js/index.js
+++ js/index.js
@@ -88,11 +88,8 @@ class App extends Component {
 					</form>
 				</header>
 				<section className="main">
-					<input id="toggle-all" className="toggle-all" type="checkbox" />
-					<label
-						htmlFor="toggle-all"
-						onClick={() => this.onToggleAll()}
-					>
+					<input id="toggle-all" className="toggle-all" type="checkbox" onChange={() => this.onToggleAll()} />
+					<label htmlFor="toggle-all">
 						Mark all as complete
 					</label>
 					<ul className="todo-list">
