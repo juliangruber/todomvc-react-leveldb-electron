6b1d502c4f5ed4da26e05b8ead82f21f0bfdc718
Julian Gruber <julian@juliangruber.com>
2017-12-18 14:24:32 +0100

    webpack: tabs

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  entry: './src/index.js',
-  target: 'electron',
-  output: {
-    filename: 'static/build.js',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          `${__dirname}/node_modules/react-level-list`,
-          `${__dirname}/node_modules/react-level-count`,
-          `${__dirname}/src`
-        ],
-        loader: 'babel-loader',
-        query: {
-          presets: ['react']
-        }
-      }
-    ]
-  },
-  externals: {
-    leveldown: true
-  }
-}
+	entry: './src/index.js',
+	target: 'electron',
+	output: {
+		filename: 'static/build.js',
+		libraryTarget: 'commonjs2'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: [
+					`${__dirname}/node_modules/react-level-list`,
+					`${__dirname}/node_modules/react-level-count`,
+					`${__dirname}/src`
+				],
+				loader: 'babel-loader',
+				query: {
+					presets: ['react']
+				}
+			}
+		]
+	},
+	externals: {
+		leveldown: true
+	}
+};
